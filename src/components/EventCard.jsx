e4378244bd79d1abdea4c8077796bb2c81739a35
benjasmines <143446989+Benjasmines@users.noreply.github.com>
2025-11-12 22:54:44 -0300

    avance propio del benjas
    generé el archivo .env y subí el exaple de como va el .env
    saludos <3

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -2,61 +2,28 @@ import { Link } from "react-router-dom";
 
 function EventCard({ event }) {
   return (
-    <div
-      className="event-card"
-      style={{
-        background: "#2a2a2a",
-        padding: "1rem",
-        borderRadius: "12px",
-        boxShadow: "0 4px 10px rgba(0,0,0,0.3)",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-        transition: "transform 0.2s, box-shadow 0.2s",
-      }}
-    >
+    <div className="flex flex-col gap-15 bg-gray-800 rounded-xl border-2 border-gray-900 shadow-lg overflow-hidden transform transition-transform hover:scale-105 hover:shadow-2xl">
       <img
         src={event.image}
         alt={event.name}
-        style={{
-          width: "100%",
-          height: "180px",
-          objectFit: "cover",
-          borderRadius: "10px",
-          marginBottom: "1rem",
-        }}
+        className="w-full h-48 object-cover"
       />
-      <div style={{ flex: 1 }}>
-        <h2 style={{ margin: "0.5rem 0", color: "#fff" }}>{event.name}</h2>
-        <p style={{ margin: "0.3rem 0", color: "#ccc" }}>📍 {event.location}</p>
-        <p style={{ margin: "0.3rem 0", color: "#aaa" }}>
-          🗓️ {new Date(event.date).toLocaleString()}
-        </p>
-        <p style={{
-          margin: "0.3rem 0",
-          color: "#999",
-          fontSize: "0.9rem"
-        }}>
-          🎫 {event.category}
-        </p>
+      <div className="p-4 flex flex-col justify-between">
+        <div>
+          <h2 className="text-2xl font-semibold text-white mb-2">{event.name}</h2>
+          <p className="text-xl text-gray-400 text-sm mb-1">📍 {event.location}</p>
+          <p className="text-xl text-gray-500 text-sm mb-1">
+            🗓️ {new Date(event.date).toLocaleString()}
+          </p>
+          <p className="text-xl text-gray-500 text-sm">🎫 {event.category}</p>
+        </div>
+        <Link
+          to={`/event/${event.id}`}
+          className="mt-4 inline-block bg-sky-500 hover:bg-sky-600 text-white text-center font-medium py-2 px-4 rounded-lg transition-colors"
+        >
+          Ver Detalle
+        </Link>
       </div>
-      <Link
-        to={`/event/${event.id}`}
-        style={{
-          display: "inline-block",
-          marginTop: "1rem",
-          color: "#fff",
-          background: "#646cff",
-          padding: "0.5rem 1rem",
-          borderRadius: "8px",
-          textDecoration: "none",
-          transition: "background 0.3s",
-        }}
-        onMouseOver={(e) => (e.target.style.background = "#535bf2")}
-        onMouseOut={(e) => (e.target.style.background = "#646cff")}
-      >
-        Ver Detalle
-      </Link>
     </div>
   );
 }
