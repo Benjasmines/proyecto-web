be95597aca3e43ca6b081476c9eb957fdf73991c
oliam435 <alojamega4@gmail.com>
2025-11-12 23:29:04 -0300

    arreglando error en la branch

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function EventCard({ event }) {
   return (
-    <div className="flex flex-col gap-15 bg-gray-800 rounded-xl shadow-lg overflow-hidden transform transition-transform hover:scale-105 hover:shadow-2xl">
+    <div className="flex flex-col gap-15 bg-gray-800 rounded-xl border-2 border-gray-900 shadow-lg overflow-hidden transform transition-transform hover:scale-105 hover:shadow-2xl">
       <img
         src={event.image}
         alt={event.name}
@@ -10,15 +10,15 @@ function EventCard({ event }) {
       />
       <div className="p-4 flex flex-col justify-between">
         <div>
-          <h2 className="text-xl font-semibold text-white mb-2">{event.name}</h2>
-          <p className="text-gray-400 text-sm mb-1">📍 {event.location}</p>
-          <p className="text-gray-500 text-sm mb-1">
+          <h2 className="text-2xl font-semibold text-white mb-2">{event.name}</h2>
+          <p className="text-xl text-gray-400 text-sm mb-1">📍 {event.location}</p>
+          <p className="text-xl text-gray-500 text-sm mb-1">
             🗓️ {new Date(event.date).toLocaleString()}
           </p>
-          <p className="text-gray-500 text-sm">🎫 {event.category}</p>
+          <p className="text-xl text-gray-500 text-sm">🎫 {event.category}</p>
         </div>
         <Link
-          to={`/event/${event._id}`}
+          to={`/event/${event.id}`}
           className="mt-4 inline-block bg-sky-500 hover:bg-sky-600 text-white text-center font-medium py-2 px-4 rounded-lg transition-colors"
         >
           Ver Detalle
